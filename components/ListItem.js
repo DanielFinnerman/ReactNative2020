8aa3914e415404a70a1ba6b37c648f46660cb4f9
Daniel Finnerman <danu95d@hotmail.com>
2020-01-15 09:47:34 +0200

    paska

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,4 +1,4 @@
-import { Image, Text, TouchableOpacity, View } from "react-native";
+import { Image, Text, TouchableOpacity, View, StyleSheet } from "react-native";
 import React from "react";
 import { PropTypes } from "prop-types";
 
