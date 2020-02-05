c0a2460d62f900f622c88b4fa3b11881c817486d
Daniel Finnerman <danu95d@hotmail.com>
2020-02-05 14:49:36 +0200

    plaod

--- hooks/UploadHooks.js
+++ hooks/UploadHooks.js
@@ -1,55 +1,78 @@
-import {useState} from 'react';
-import mediaAPI from './ApiHooks';
-
-const {uploadFile} = mediaAPI();
+import {useState, useContext} from 'react';
+import {AsyncStorage} from 'react-native';
+import {MediaContext} from '../contexts/MediaContext';
+import {fetchGET} from './APIHooks';
 
 const useUploadForm = () => {
+  const [media, setMedia] = useContext(MediaContext);
   const [inputs, setInputs] = useState({});
-  // upload form event handlers
+  const [errors, setErrors] = useState({});
+
   const handleTitleChange = (text) => {
-    setInputs((inputs) => ({
-      ...inputs,
-      title: text,
-    }));
+    setInputs((inputs) =>
+      ({
+        ...inputs,
+        title: text,
+      }));
   };
+
   const handleDescriptionChange = (text) => {
-    setInputs((inputs) => ({
-      ...inputs,
-      description: text,
-    }));
+    setInputs((inputs) =>
+      ({
+        ...inputs,
+        description: text,
+      }));
   };
 
-  const handleUpload = (file) => {
-    const fd = new FormData();
-    const filename = file.uri.split('/').pop();
+  const handleUpload = async (file) => {
+    try {
 
-    // Infer the type of the image
+    const filename = file.uri.split('/').pop();
     const match = /\.(\w+)$/.exec(filename);
-    let type = '';
-    if (file.type === 'image') {
-      type = match ? `image/${match[1]}` : `image`;
-      // fix jpg mimetype
-      if (type === 'image/jpg') {
-        type = 'image/jpeg';
-      }
-    } else {
-      type = match ? `video/${match[1]}` : `video`;
+    let type = match ? `image/${match[1]}` : `image`;
+    if (type === 'image/jpg') {
+      type = 'image/jpeg';
     }
 
-    // Upload the image using the fetch and FormData APIs
-    // Assume "photo" is the name of the form field the server expects
-    fd.append('file', {uri: file.uri, name: filename, type});
+    const fd = new FormData();
     fd.append('title', inputs.title);
     fd.append('description', inputs.description);
-    console.log(uploadFile(fd));
+    fd.append('file', {uri: file.uri, name: filename, type});
+
+    const token = await AsyncStorage.getItem('userToken');
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: {
+        'x-access-token': token,
+      },
+      body: fd,
+    };
+
+
+      const result = await fetch('http://media.mw.metropolia.fi/wbma/media', fetchOptions);
+      const json = await result.json();
+      console.log('upload result:', json);
+      if(json.file_id) {
+        const json = await fetchGET('media/all');
+        const result = await Promise.all(json.files.map(async (item) => {
+          return await fetchGET('media', item.file_id);
+        }));
+        setMedia(result);
+        navigation.push('Home');
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
   };
 
   return {
     handleTitleChange,
     handleDescriptionChange,
-    handleUpload,
     inputs,
+    errors,
+    setErrors,
+    handleUpload
   };
 };
-
 export default useUploadForm;
--- views/Upload.js
+++ views/Upload.js
@@ -1,20 +1,50 @@
-import React, {useState} from 'react';
-import {
-  Content,
-  Form,
-  Button,
-  Text,
-  Item,
-} from 'native-base';
-import {
-  AsyncStorage,
-} from 'react-native';
-import PropTypes from 'prop-types';
-import {fetchGET, fetchPOST} from '../hooks/APIHooks';
-import FormTextInput from '../components/FormTextInput';
 
+import React, {useState, useEffect} from 'react';
+import {Image} from 'react-native';
+import {Form, Button, Text, Content} from 'native-base';
+import FormTextInput from '../components/FormTextInput';
+import PropTypes from 'prop-types';
+import useUploadForm from '../hooks/UploadHooks';
+import * as ImagePicker from 'expo-image-picker';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
 
 const Upload = (props) => {
+  const [image, setImage] = useState(image = null);
+
+  const {
+    inputs,
+    handleTitleChange,
+    handleDescriptionChange,
+    handleUpload,
+  } = useUploadForm();
+
+   const getPermissionAsync = async () => {
+    if (Constants.platform.ios) {
+      const {status} = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== 'granted') {
+        alert('Sorry, we need camera roll permissions to make this work!');
+      }
+    }
+  };
+
+  const pickImage = async () => {
+    const result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setFile(result);
+    }
+  };
+
+  useEffect(() => {
+    getPermissionAsync();
+  }, []);
 
   return (
       <Content>
@@ -22,26 +52,34 @@ const Upload = (props) => {
           <Item>
             <FormTextInput
               placeholder='Title'
+              onChangeText={handleTitleChange}
+              value={inputs.title}
             />
           </Item>
           <Item>
             <FormTextInput
               placeholder='Description'
+              onChangeText={handleDescriptionChange}
+              value={inputs.description}
             />
           </Item>
-          <Button full>
+          {image &&
+          <Image source={{ uri: image.uri }} style={{ width: '100%', height: deviceHeight / 3 }} />}
+          <Button full onPress={pickImage}>
             <Text>Select file</Text>
           </Button>
           <Button dark full>
           <Text>Reset form</Text>
         </Button>
-          <Button full>
+          <Button full onPress={() => {
+            handleUpload(image);
+          }}>
             <Text>Upload</Text>
           </Button>
         </Form>
       </Content>
   );
-};
+}
 
 // proptypes here
 Upload.propTypes = {
